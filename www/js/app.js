53b8fab2fa43cd5aefda553aa7182674519ab804
aman kothari <amankothari85@gmail.com>
2016-01-30 17:42:13 +0530

    ass

--- www/js/app.js
+++ www/js/app.js
@@ -5,130 +5,138 @@
 // the 2nd parameter is an array of 'requires'
 // 'starter.services' is found in services.js
 // 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic','ngCordova','ionic.service.core', 'starter.controllers', 'starter.services'])
+angular.module('starter', ['ionic', 'ngCordova', 'ionic.service.core', 'starter.controllers', 'starter.services'])
 
-.run(function ($ionicPlatform, $rootScope, $cordovaNetwork) {
-  $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-      // for form inputs)
+.run(function ($ionicPlatform, $rootScope, $cordovaNetwork, $ionicPopup) {
+    $ionicPlatform.ready(function () {
+        // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+        // for form inputs)
 
-      var io = Ionic.io();
-      var push = new Ionic.Push({
-          "onNotification": function (notification) {
-              alert('Received push notification!');
-          },
-          "pluginConfig": {
-              "android": {
-                  "iconColor": "#0000FF"
+        var io = Ionic.io();
+        var push = new Ionic.Push({
+            "onNotification": function (notification) {
+                alert('Received push notification!');
+            },
+            "pluginConfig": {
+                "android": {
+                    "iconColor": "#0000FF"
+                }
+            }
+        });
+        $rootScope.$on('$cordovaNetwork:online', function (event, networkState) {
+            alert('online');
+            //$scope.isOnline = true;
+            //$scope.network = $cordovaNetwork.getNetwork();
+
+            //$scope.$apply();
+        })
+
+        // listen for Offline event
+        $rootScope.$on('$cordovaNetwork:offline', function (event, networkState) {
+            alert('offline');
+            $ionicPopup.confirm({
+                title: "Internet Disconnected",
+                content: "The internet is disconnected on your device."
+            }).then(function (result) {
+                if (!result) {
+                    ionic.Platform.exitApp();
+                }
+            });
+
+        })
+        var user = Ionic.User.current();
+
+        if (!user.id) {
+            user.id = Ionic.User.anonymousId();
+        }
+
+        // Just add some dummy data..
+        user.set('name', 'Aman');
+        user.set('bio', 'This is android demo.');
+        user.save();
+        push.register(function (data) {
+            push.addTokenToUser(user);
+            user.save();
+        });
+        var success = function (response) {
+            alert('user was saved');
+        };
+
+        var failure = function (error) {
+            alert('user was NOT saved :' + error);
+        };
+
+
+
+        if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
+            cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+            cordova.plugins.Keyboard.disableScroll(true);
+
+        }
+        if (window.StatusBar) {
+            // org.apache.cordova.statusbar required
+            StatusBar.styleDefault();
+        }
+    });
+})
+
+.config(function ($stateProvider, $urlRouterProvider) {
+
+    // Ionic uses AngularUI Router which uses the concept of states
+    // Learn more here: https://github.com/angular-ui/ui-router
+    // Set up the various states which the app can be in.
+    // Each state's controller can be found in controllers.js
+    $stateProvider
+
+    // setup an abstract state for the tabs directive
+      .state('tab', {
+          url: '/tab',
+          abstract: true,
+          templateUrl: 'templates/tabs.html'
+      })
+
+    // Each tab has its own nav history stack:
+
+    .state('tab.dash', {
+        url: '/dash',
+        views: {
+            'tab-dash': {
+                templateUrl: 'templates/tab-dash.html',
+                controller: 'DashCtrl'
+            }
+        }
+    })
+
+    .state('tab.chats', {
+        url: '/chats',
+        views: {
+            'tab-chats': {
+                templateUrl: 'templates/tab-chats.html',
+                controller: 'ChatsCtrl'
+            }
+        }
+    })
+      .state('tab.chat-detail', {
+          url: '/chats/:chatId',
+          views: {
+              'tab-chats': {
+                  templateUrl: 'templates/chat-detail.html',
+                  controller: 'ChatDetailCtrl'
               }
           }
-      });
-      $rootScope.$on('$cordovaNetwork:online', function (event, networkState) {
-          alert('online');
-          //$scope.isOnline = true;
-          //$scope.network = $cordovaNetwork.getNetwork();
-
-          //$scope.$apply();
       })
 
-      // listen for Offline event
-      $rootScope.$on('$cordovaNetwork:offline', function (event, networkState) {
-          alert('offline');
-          
-      })
-      var user = Ionic.User.current();
-
-      if (!user.id) {
-          user.id = Ionic.User.anonymousId();
-      }
-
-      // Just add some dummy data..
-      user.set('name', 'Aman');
-      user.set('bio', 'This is android demo.');
-      user.save();
-      push.register(function (data) {
-          push.addTokenToUser(user);
-          user.save();
-      });
-      var success = function(response) {
-          alert('user was saved');
-      };
-
-      var failure = function(error) {
-          alert('user was NOT saved :' + error);
-      };
-
-      
-
-    if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-      cordova.plugins.Keyboard.disableScroll(true);
-
-    }
-    if (window.StatusBar) {
-      // org.apache.cordova.statusbar required
-      StatusBar.styleDefault();
-    }
-  });
-})
- 
-.config(function($stateProvider, $urlRouterProvider) {
-
-  // Ionic uses AngularUI Router which uses the concept of states
-  // Learn more here: https://github.com/angular-ui/ui-router
-  // Set up the various states which the app can be in.
-  // Each state's controller can be found in controllers.js
-  $stateProvider
-
-  // setup an abstract state for the tabs directive
-    .state('tab', {
-    url: '/tab',
-    abstract: true,
-    templateUrl: 'templates/tabs.html'
-  })
-
-  // Each tab has its own nav history stack:
-
-  .state('tab.dash', {
-    url: '/dash',
-    views: {
-      'tab-dash': {
-        templateUrl: 'templates/tab-dash.html',
-        controller: 'DashCtrl'
-      }
-    }
-  })
-
-  .state('tab.chats', {
-      url: '/chats',
-      views: {
-        'tab-chats': {
-          templateUrl: 'templates/tab-chats.html',
-          controller: 'ChatsCtrl'
+    .state('tab.account', {
+        url: '/account',
+        views: {
+            'tab-account': {
+                templateUrl: 'templates/tab-account.html',
+                controller: 'AccountCtrl'
+            }
         }
-      }
-    })
-    .state('tab.chat-detail', {
-      url: '/chats/:chatId',
-      views: {
-        'tab-chats': {
-          templateUrl: 'templates/chat-detail.html',
-          controller: 'ChatDetailCtrl'
-        }
-      }
-    })
+    });
 
-  .state('tab.account', {
-    url: '/account',
-    views: {
-      'tab-account': {
-        templateUrl: 'templates/tab-account.html',
-        controller: 'AccountCtrl'
-      }
-    }
-  });
-
-  // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/tab/dash');
+    // if none of the above states are matched, use this as the fallback
+    $urlRouterProvider.otherwise('/tab/dash');
 
 });
